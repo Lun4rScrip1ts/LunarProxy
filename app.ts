706c85dbf4e94afc04bb40f668074b5fca724366
Lun4rScrip1ts <xv8trw9zgk@privaterelay.appleid.com>
2026-08-19 10:17:11 -0400

    app.ts

---
+++ app.ts
@@ -0,0 +1,9 @@
+import { Hono } from "hono";
+const app = new hono(); 
+const LUNAR_CHROME - ' 
+<style id="lunar-cursor-style">
+    html, body, a, button, input, textarea,
+  select, [role="button"] {cursor: none !
+important; }
+  #lunar-crosshair { 
+    Posistion: fixed;
